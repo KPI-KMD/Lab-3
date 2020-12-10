04d36ca3d72ad52a31b48abfec32c7655b249939
Kyrylo Andreichenko <42568173+andreichenko256@users.noreply.github.com>
2020-12-10 16:41:51 +0200

    Update db_test.go

--- server/db/db_test.go
+++ server/db/db_test.go
@@ -6,7 +6,7 @@ func TestDbConnection_ConnectionURL(t *testing.T) {
 	conn := &Connection{
 		DbName:     "Multimedia_class",
 		User:       "postgres",
-		Password:   "admin",
+		Password:   "",
 		Host:       "localhost",
 		DisableSSL: true,
 	}
