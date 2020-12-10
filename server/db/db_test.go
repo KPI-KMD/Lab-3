73bc2b49d8baadf5bc8121bbd285e54fc1e8184d
Kyrylo Andreichenko <42568173+andreichenko256@users.noreply.github.com>
2020-12-10 16:07:18 +0200

    db_test.go added

--- server/db/db_test.go
+++ server/db/db_test.go
@@ -1,3 +1,16 @@
 package db
 
-import ()
+import "testing"
+
+func TestDbConnection_ConnectionURL(t *testing.T) {
+	conn := &Connection{
+		DbName:     "Multimedia_class",
+		User:       "postgres",
+		Password:   "admin",
+		Host:       "localhost",
+		DisableSSL: true,
+	}
+	if conn.ConnectionURL() != "postgres://postgres:admin@localhost/Multimedia_class?sslmode=disable" {
+		t.Error("Unexpected connection string")
+	}
+}
